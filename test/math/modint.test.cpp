109bfb4b5d861d0b98a65be9ae157c4147ddf622
Ricetawara459 <koro720@gmail.com>
2026-06-23 12:13:20 +0900

    Update modint.test.cpp

--- test/math/modint.test.cpp
+++ test/math/modint.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_products"
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product"
 
 #include <iostream>
 #include <vector>
@@ -10,18 +10,34 @@ int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    int n;
-    if (!(cin >> n)) return 0;
+    int n, m, k;
+    if (!(cin >> n >> m >> k)) return 0;
 
-    vector<mint998244353> a(n), b(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) cin >> b[i];
+    vector<vector<mint998244353>> a(n, vector<mint998244353>(m));
+    vector<vector<mint998244353>> b(m, vector<mint998244353>(k));
 
-    mint998244353 ans = 0;
     for (int i = 0; i < n; i++) {
-        ans += a[i] * b[i];
+        for (int j = 0; j < m; j++) cin >> a[i][j];
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < k; j++) cin >> b[i][j];
     }
 
-    cout << ans << "\n";
+    vector<vector<mint998244353>> c(n, vector<mint998244353>(k, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int h = 0; h < k; h++) {
+                c[i][h] += a[i][j] * b[j][h];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << c[i][j] << (j + 1 == k ? "" : " ");
+        }
+        cout << "\n";
+    }
+
     return 0;
 }

047739ddd9ad4033ee40b88ef60e31129d7621ac
Ricetawara459 <koro720@gmail.com>
2026-06-23 18:43:04 +0900

    a

---
+++ test/math/convolution.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+
+#include <iostream>
+#include <vector>
+#include "../../src/math/convolution.hpp"
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+
+    vector<long long> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    auto c = convolution(a, b);
+
+    for (int i = 0; i < n + m - 1; i++) {
+        cout << c[i] << (i == n + m - 2 ? "" : " ");
+    }
+    cout << "\n";
+
+    return 0;
+}
